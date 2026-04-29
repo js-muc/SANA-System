f3fc68483e90136bf2ed4a407f4ac54fe7a2bc12
js-muc <165466095+js-muc@users.noreply.github.com>
2026-04-29 06:29:40 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,6 @@ function SuperAdminRoute({ children }: { children: React.ReactNode }) {
 
 function ProtectedRoutes() {
   const { user, profile, loading } = useAuth();
-  console.log("PROTECTED ROUTE:", { user, profile, loading });
 
 if (loading || !profile) {
   return (
